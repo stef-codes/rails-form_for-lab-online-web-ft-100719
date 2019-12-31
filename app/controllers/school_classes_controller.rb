8f9549657269d5bd78c740e94ef1fcb276adb67d
Stefanie <stefcodes@gmail.com>
2019-12-30 23:02:52 -0800

    Done.

---
+++ app/controllers/school_classes_controller.rb
@@ -0,0 +1,4 @@
+class SchoolClassesController < ActionController::Base
+    # Prevent CSRF attacks by raising an exception.
+    # For APIs, you may want to use :null_session instead.
+  end
---
+++ db/migrate/20191231002446_school_class.rb
@@ -0,0 +1,10 @@
+class SchoolClass < ActiveRecord::Migration[5.0]
+  def change
+    create_table :school_class do |t|
+      t.string :title
+      t.integer :room_number
+
+      t.timestamps null: false
+    end
+  end
+end
