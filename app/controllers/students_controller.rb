014ce96fb8053383f4f7ecf870670382b211976c
Stefanie <stefcodes@gmail.com>
2019-12-31 20:12:02 -0800

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,5 +1,34 @@
 class StudentsController < ActionController::Base
     # Prevent CSRF attacks by raising an exception.
     # For APIs, you may want to use :null_session instead.
-    
-  end
+    def index
+      @students = Student.all
+    end
+  
+    def show
+      @student = Student.find(params[:id])
+    end
+  
+    def new
+      @student = Student.new
+    end
+  
+    def create
+      @student = Student.new
+      @student.first_name = params[:first_name]
+      @student.last_name = params[:last_name]
+      @student.save
+      redirect_to student_path(@student)
+    end
+  
+    # add edit and update methods here
+    def edit
+      @student = Student.find(params[:id])
+    end
+  
+    def update
+      @student = Student.find(params[:id])
+      @student.update(first_name: params[:student][:first_name], last_name: params[:student][:last_name])
+      redirect_to student_path(@student)
+    end
+end
