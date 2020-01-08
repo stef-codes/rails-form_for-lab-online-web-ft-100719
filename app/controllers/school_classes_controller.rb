a513a4db862d48bc729371a571f9714cdc46c201
Stefanie <stefcodes@gmail.com>
2020-01-07 20:57:48 -0800

    Done.

--- app/controllers/school_classes_controller.rb
+++ app/controllers/school_classes_controller.rb
@@ -1,4 +1,4 @@
-class SchoolClassController < ActionController::Base
+class SchoolClassesController < ActionController::Base
     # Prevent CSRF attacks by raising an exception.
     # For APIs, you may want to use :null_session instead.
     def index
